abcfe662d413fa3f91fbc088fdda53a021be5d73
Naveen Regulus <navynregulus@gmail.com>
2020-08-09 16:13:43 +0530

    Using Mongo Atlas instead of local mongo

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ var indexRoutes = require("./routes/index")
 var campgroudRoutes = require("./routes/campgrounds")
 var commentRoutes = require("./routes/comments")
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+// mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true
+// })
+// .then(() => console.log('Connected to DB!'))
+// .catch(error => console.log(error.message));
+mongoose.connect('mongodb+srv://yelpcamp:abc12345@cluster0.cg0lt.mongodb.net/yelpcamp?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
